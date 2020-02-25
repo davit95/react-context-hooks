848e02fff7c5ebf9cb8acb9c82beff1c183e57d3
davit95 <davstepanyan95@gmail.com>
2020-02-25 14:13:12 +0400

    added other versions for context app in Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,47 @@
 import React, { useContext } from 'react';
 import { BookContext } from '../contexts/BookContext';
  
+// class Navbar extends PureComponent {
+//   // static contextType = ThemeContext;
+//   render() {
+//     return ( 
+//       <AuthContext.Consumer>{(authContext) => (
+//         <ThemeContext.Consumer>{(themeContext) => {
+//           const { isLightTheme, light, dark } = themeContext;
+//           const theme = isLightTheme ? light : dark;
+//           const { isAuthenticated, toggleAuth } = authContext;
+//           return (
+//             <nav style={{ background: theme.ui, color: theme.syntax }}>
+//               <h1>Context App</h1>
+//               <div onClick={toggleAuth}>
+//                 { isAuthenticated ? 'Logged in' : 'Logged out' }
+//               </div>
+//               <ul>
+//                 <li>Home</li>
+//                 <li>About</li>
+//                 <li>Contact</li>
+//               </ul>
+//             </nav>
+//           )
+//         }}</ThemeContext.Consumer>
+//       )}
+//       </AuthContext.Consumer>
+//     );
+//   }
+// }
+
+// export default class Navbar extends PureComponent {
+//   static contextType = BookContext
+//   render() {
+//     const { boooks } = this.context
+//     return (
+//       <div>
+//         {books.map(book => console.log(book))}  
+//       </div>
+//     )
+//   }
+// }
+
 const Navbar = () => {
   const { books } = useContext(BookContext)
   return (
